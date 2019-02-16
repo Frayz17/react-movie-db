604b1bb96b7194a7a1dea1817a8e6191814d4535
Andrey <andrydmit@gmail.com>
2019-02-16 21:50:00 +0200

    Creating Movie component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react'
+import Movie from './components/Movie'
 import logo from './logo.svg'
 import './App.css'
 
-class App extends Component {
+const movies = [
+  {
+    id: 1,
+    title: 'Titanik'
+  },
+  {
+    id: 2,
+    title: 'Superman'
+  },
+  {
+    id: 3,
+    title: 'Batman'
+  }
+]
 
+class App extends Component {
   render () {
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
         </header>
+        {movies.map(movie => {
+          return <Movie title={movie.title} key={movie.id} />
+        })}
       </div>
     )
   }
 }
-
-
 
 export default App
