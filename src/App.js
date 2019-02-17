6c50195808da61aef826aeb5489e1f6d830924f6
Andrey <andrydmit@gmail.com>
2019-02-18 00:49:07 +0200

    Fetching data from movie DB

--- src/App.js
+++ src/App.js
@@ -3,31 +3,32 @@ import Movie from './components/Movie'
 import logo from './logo.svg'
 import './App.css'
 
-const movies = [
-  {
-    id: 1,
-    title: 'Titanik',
-    desc: 'ship movie'
-  },
-  {
-    id: 2,
-    title: 'Superman'
-  },
-  {
-    id: 3,
-    title: 'Batman'
-  }
-]
-
 class App extends Component {
+  state = {
+    movies: []
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch()
+      const movies = await res.json()
+      
+      this.setState({
+        movies: movies.results
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   render () {
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
         </header>
-        {movies.map(movie => {
-          return <Movie movie={movie} desc={movie.desc} key={movie.id} />
+        {this.state.movies.map(movie => {
+          return <Movie movie={movie} key={movie.id} />
         })}
       </div>
     )
